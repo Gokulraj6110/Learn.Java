0ba675947c0f51923884e930ed67793e44b741df
Gokulraj6110 <gokulraj6110@gmail.com>
2021-10-15 19:19:30 +0530

    Initial commit

---
+++ src/com/Gokulraj/hello_world.java
@@ -0,0 +1,9 @@
+package com.Gokulraj;
+
+public class hello_world {
+
+    public static void main(String[] args) {
+	//First program of all the programming language
+        System.out.println("Hello world..!");
+    }
+}
