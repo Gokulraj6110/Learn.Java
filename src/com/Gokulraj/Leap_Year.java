5341e33055d438895704b7cff784fd123e033a85
Gokulraj6110 <gokulraj6110@gmail.com>
2021-10-16 15:25:39 +0530

    Array Leap_Year

--- src/com/Gokulraj/Leap_Year.java
+++ src/com/Gokulraj/Leap_Year.java
@@ -12,19 +12,18 @@ public class Leap_Year {
         if(year % 4 == 0) {
             if( year % 100 == 0) {
                 if (year % 400 != 0) {
-                } else {
+                }
+                else {
                     isLeap = true;
                 }
             }
             else
                 isLeap = true;
-                }
+        }
 
         if(isLeap)
                     System.out.println(year + " is a Leap Year.");
                 else
                     System.out.println(year + " is not a Leap Year.");
-
-
     }
 }
