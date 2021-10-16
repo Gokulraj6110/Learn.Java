c727af0fadfa2766ac514a4e37ab55b6cfba6255
Gokulraj6110 <gokulraj6110@gmail.com>
2021-10-16 21:10:32 +0530

    Array Simple_Interest, Operators_2

---
+++ src/com/Gokulraj/Math_Functions.java
@@ -0,0 +1,16 @@
+package com.Gokulraj;
+
+public class Math_Functions {
+    public static void main(String[] args){
+        int num = 10;
+        float num1 = 20.532F;
+
+        System.out.println(Math.abs(num));
+        System.out.println(Math.ceil(num1));
+        System.out.println(Math.floor(num1));
+        System.out.println(Math.exp(num));
+        System.out.println(Math.log(num));
+        System.out.println(Math.max(num,num1));
+        System.out.println(Math.min(num,num1));
+    }
+}
