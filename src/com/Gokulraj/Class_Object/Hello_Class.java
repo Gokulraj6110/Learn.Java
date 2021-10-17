811d03504ca41573e9b3e7c20b1093195c35d5ac
Gokulraj6110 <gokulraj6110@gmail.com>
2021-10-17 10:29:36 +0530

    Array Simple_Interest, Operators_2

---
+++ src/com/Gokulraj/Class_Object/Hello_Class.java
@@ -0,0 +1,7 @@
+package com.Gokulraj.Class_Object;
+
+public class Hello_Class {
+    public void PrintHello() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/com/Gokulraj/Class_Object/Main.java
@@ -0,0 +1,8 @@
+package com.Gokulraj.Class_Object;
+
+public class Main {
+    public static void main(String[] args){
+        Hello_Class HelloObject = new Hello_Class();
+        HelloObject.PrintHello();
+    }
+}
